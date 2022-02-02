e37aaf9e99329d395fe75db9ce1d5f8a8ef76907
abdi <iabdullahbintahir@gmail.com>
2022-02-02 22:36:52 +0500

    add model

--- api/main.py
+++ api/main.py
@@ -3,8 +3,8 @@ import os
 import pandas as pd
 
 WORKING_DIR = os.getcwd()
-DATA_DIR = os.path.join(WORKING_DIR, 'api/data')
-
+DATA_DIR = os.path.join(WORKING_DIR, 'data')
+# print(f'{DATA_DIR}/positions_train_out.csv')
 app = FastAPI()
 
 @app.get('/')
@@ -13,14 +13,19 @@ def home():
         'Title': 'Artificial Intelligence Empowered Fog Computing',
         'Code': 'https://www.github.com/shy-tan/final-year-project',
         'Authors': ['FA18-BEE-127', 'FA18-BEE-009', 'FA18-BEE-021'],
+        'Endpoints': {
+            '/train-data': 'Preprocessed train data for both speed and position',
+            '/test-data': 'Preprocessed test data for both speed and position',
+            '/raw-data': 'Source data'
+        }
         }
 
 
 @app.get('/train-data')
 def train_data():
-    pos_train_df = pd.read_csv(f'{CACHE}/positions_train_out.csv')
+    pos_train_df = pd.read_csv(f'{DATA_DIR}/positions_train_out.csv')
 
-    speeds_train_df = pd.read_csv(f'{CACHE}/speeds_train_out.csv')
+    speeds_train_df = pd.read_csv(f'{DATA_DIR}/speeds_train_out.csv')
 
     return {
         'speeds_train': speeds_train_df.to_dict(),
@@ -29,10 +34,18 @@ def train_data():
 
 @app.get('/test-data')
 def test_data():
-    pos_test_df = pd.read_csv(f'{CACHE}/positions_test_out.csv')
-    speeds_test_df = pd.read_csv(f'{CACHE}/speeds_test_out.csv')
+    pos_test_df = pd.read_csv(f'{DATA_DIR}/positions_test_out.csv')
+    speeds_test_df = pd.read_csv(f'{DATA_DIR}/speeds_test_out.csv')
 
     return {
         'speeds_test': speeds_test_df.to_dict() ,
         'positions_test': pos_test_df.to_dict()
+    }
+
+@app.get('/raw-data')
+def raw_data():
+    raw_df = pd.read_csv(f'{DATA_DIR}/sumo.csv')
+
+    return {
+        'raw_data' : raw_df.to_dict(),
     }
