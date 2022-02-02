a6f43fc2f862f259a722cb76e7c6300880f9bdcf
abdi <iabdullahbintahir@gmail.com>
2022-02-02 15:34:15 +0500

    update gitignore

---
+++ api/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+import os
+import pandas as pd
+
+WORKING_DIR = os.getcwd()
+BASE_DIR = os.path.dirname(WORKING_DIR)
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+CACHE = os.path.join(DATA_DIR, 'cache')
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {
+        'Title': 'Artificial Intelligence Empowered Fog Computing',
+        'Code': 'https://www.github.com/shy-tan/final-year-project',
+        'Authors': ['FA18-BEE-127', 'FA18-BEE-009', 'FA18-BEE-021'],
+        }
+
+
+@app.get('/train-data')
+def train_data():
+    pos_train_df = pd.read_csv(f'{CACHE}/positions_train_out.csv')
+
+    speeds_train_df = pd.read_csv(f'{CACHE}/speeds_train_out.csv')
+
+    return {
+        'speeds_train': speeds_train_df.to_dict(),
+        'positions_train': pos_train_df.to_dict(),
+    }
+
+@app.get('/test-data')
+def test_data():
+    pos_test_df = pd.read_csv(f'{CACHE}/positions_test_out.csv')
+    speeds_test_df = pd.read_csv(f'{CACHE}/speeds_test_out.csv')
+
+    return {
+        'speeds_test': speeds_test_df.to_dict() ,
+        'positions_test': pos_test_df.to_dict()
+    }
