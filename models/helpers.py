5a4acc671de1798151315c6251f46f39ac87b06a
Abdi <48406637+shy-tan@users.noreply.github.com>
2022-05-21 16:50:46 +0500

    update structure

--- models/helpers.py
+++ models/helpers.py
@@ -1,17 +1,19 @@
+
 import numpy as np
+
 
 class VectorNormalizer:
     def __init__(self):
         self.norm = None
-        
+
     def fit(self, dataframe):
         self.norm = np.linalg.norm(dataframe, axis=0)
         return self.norm
-    
+
     def transform(self, dataframe):
         if self.norm is not None:
             return dataframe/self.norm
-    
+
     def inverse_transform(self, dataframe, norms=None):
         if norms is not None:
             return dataframe*norms
